bfee4b6cfdccab7129627b2b2a51e7645d977cfa
Uday Sharma <sharmauday1000@gmail.com>
2026-06-10 14:27:20 +0530

    Implement Ransom Note solution in C++

---
+++ Week_2/Day_1/Ransom_Notes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int count[26] = {0};
+
+        for (int i = 0; i < magazine.size(); i++)
+            count[magazine[i] - 'a']++;
+
+        for (int i = 0; i < ransomNote.size(); i++) 
+        {
+            count[ransomNote[i] - 'a']--;
+            if (count[ransomNote[i] - 'a'] < 0)
+                return false;
+        }
+
+        return true;
+    }
+};
