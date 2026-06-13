aadd8210d5755256e21eddb0ca8f7e2d6307c74d
Uday Sharma <sharmauday1000@gmail.com>
2026-06-13 22:04:04 +0530

    Refactor containsDuplicate to use sorting method
    Refactored containsDuplicate function to use sorting for duplicate detection.

--- Week_1/Day_2/ContainDuplicates.cpp
+++ Week_1/Day_2/ContainDuplicates.cpp
@@ -1,14 +1,15 @@
-
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> seen;
-        for (int num : nums) {
-            if (seen.count(num)) {
-                return true; nd
-            }
-            seen.insert(num);
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        for(int i=0;i<n-1;i++){
+                if(nums[i]==nums[i+1]){
+                    return true;
+                    break;
+                }
         }
         return false;
+        
     }
 };
