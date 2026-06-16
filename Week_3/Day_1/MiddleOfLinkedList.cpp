458d7a0e9fb578581ca7a15e3fca20de7c9c34ac
Uday Sharma <sharmauday1000@gmail.com>
2026-06-17 04:59:44 +0530

    Add MiddleOfLinkedList.cpp with middleNode function
    Implement function to find the middle node of a linked list.

---
+++ Week_3/Day_1/MiddleOfLinkedList.cpp
@@ -0,0 +1,34 @@
+/*Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+
+ 
+
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+Example 2:
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+ */
+
+
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) 
+{
+        ListNode* slow = head;
+        ListNode* fast = head;
+        // Move fast by 2 steps and slow by 1 step
+        while (fast && fast->next) 
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        } 
+        // slow is now pointing to the middle node
+        return slow;
+    }
+};
