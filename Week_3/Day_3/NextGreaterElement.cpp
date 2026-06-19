7c5717f7220fbd5622f07eb34a000d61e00f964f
Uday Sharma <sharmauday1000@gmail.com>
2026-06-19 08:15:04 +0530

    Add NextGreaterElement class implementation
    Implement the next greater element algorithm for two arrays.

---
+++ Week_3/Day_3/NextGreaterElement.cpp
@@ -0,0 +1,63 @@
+/*
+The next greater element of some element x in an array is the first greater element that is to the right of x in the same array.
+
+You are given two distinct 0-indexed integer arrays nums1 and nums2, where nums1 is a subset of nums2.
+
+For each 0 <= i < nums1.length, find the index j such that nums1[i] == nums2[j] and determine the next greater element of nums2[j] in nums2. If there is no next greater element, then the answer for this query is -1.
+
+Return an array ans of length nums1.length such that ans[i] is the next greater element as described above.
+
+ 
+
+Example 1:
+
+Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+Output: [-1,3,-1]
+Explanation: The next greater element for each value of nums1 is as follows:
+- 4 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+- 1 is underlined in nums2 = [1,3,4,2]. The next greater element is 3.
+- 2 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+*/
+
+
+
+
+#include <vector>
+#include <stack>
+#include <unordered_map>
+
+class Solution {
+public:
+    std::vector<int> nextGreaterElement(std::vector<int>& nums1, std::vector<int>& nums2) 
+{
+        std::unordered_map<int, int> nextGreater;
+        std::stack<int> st;
+        
+        
+        for (int num : nums2) 
+        {
+            while (!st.empty() && st.top() < num) 
+            {
+                nextGreater[st.top()] = num;
+                st.pop();
+            }
+            st.push(num);
+        }
+        
+        
+        while (!st.empty())
+          {
+            nextGreater[st.top()] = -1;
+            st.pop();
+        }
+        
+        
+        std::vector<int> ans;
+        for (int num : nums1) 
+        {
+            ans.push_back(nextGreater[num]);
+        }
+        
+        return ans;
+    }
+};
